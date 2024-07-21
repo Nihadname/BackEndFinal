1fdfd8870b0d57737db5ee021130372d7bc0f44e
Nihadname <nihadcoding@gmail.com>
2024-07-22 02:31:51 +0400

    wfewfwe

--- BackEndFinal/Controllers/EventController.cs
+++ BackEndFinal/Controllers/EventController.cs
@@ -1,16 +1,20 @@
-﻿using BackEndFinal.Services.interfaces;
+﻿using BackEndFinal.Services;
+using BackEndFinal.Services.interfaces;
 using BackEndFinal.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackEndFinal.Controllers
 {
     public class EventController : Controller
     {
         private readonly IEventService _eventService;
+        private readonly ICategoryService categoryService;
 
-        public EventController(IEventService eventService)
+        public EventController(IEventService eventService, ICategoryService categoryService)
         {
             _eventService = eventService;
+            this.categoryService = categoryService;
         }
 
         public async  Task<IActionResult> Index()
@@ -32,6 +36,16 @@ namespace BackEndFinal.Controllers
             EventDetailVM eventDetail = new EventDetailVM();
             eventDetail.ProjectEvent = existedEvent;
             return View(eventDetail);
+        }
+        public async Task<IActionResult> EventsInCategory(int? id)
+        {
+            if (id == null) return BadRequest();
+            var categories = categoryService.GetAllCategoryQuery();
+            var categorieWithEvents = await categories.AsNoTracking().Include(s => s.events).ThenInclude(s => s.Images)
+                .FirstOrDefaultAsync(s=>s.Id==id);
+            if (categorieWithEvents == null) return NotFound();
+            return View(categorieWithEvents);
+
         }
     }
 }
