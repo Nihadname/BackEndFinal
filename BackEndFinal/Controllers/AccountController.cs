a18b1c1413b19d286f34f6c11a38d5076f8a02db
Nihadname <nihadcoding@gmail.com>
2024-07-22 17:01:14 +0400

    rerfref

---
+++ BackEndFinal/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndFinal.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> Register()
+        //{
+
+        //}
+    }
+}
