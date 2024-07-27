6b75b4ac449bf246aa48391f1047766d956a7935
Nihadname <nihadcoding@gmail.com>
2024-07-27 06:49:42 +0400

    fwfew

---
+++ BackEndFinal/Areas/AdminArea/Controllers/RequestedBuyingsController.cs
@@ -0,0 +1,66 @@
+﻿using BackEndFinal.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BackEndFinal.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class RequestedBuyingsController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public RequestedBuyingsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var allOfThem = _appDbContext.CourseRequests.AsNoTracking().ToList();
+            return View(allOfThem);
+        }
+
+        [HttpGet]
+        public IActionResult SetAppointmentTime(int? Id)
+        {
+            if (Id == null) return BadRequest("Request ID cannot be null.");
+            var courseRequest = _appDbContext.CourseRequests.FirstOrDefault(x => x.Id == Id);
+            if (courseRequest == null)
+            {
+                return NotFound("Course request not found.");
+            }
+
+            return View(courseRequest);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SetAppointmentTime(int? Id, DateTime appointmentTime)
+        {
+            if(Id is null) return BadRequest();
+            var courseRequest = await _appDbContext.CourseRequests.FirstOrDefaultAsync(s=>s.Id==Id);
+            if (courseRequest == null)
+            {
+                return NotFound("Course request not found.");
+            }
+
+            courseRequest.AppointmentTime = appointmentTime;
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> ApproveRequest(int requestId)
+        {
+            var courseRequest = await _appDbContext.CourseRequests.FindAsync(requestId);
+            if (courseRequest == null)
+            {
+                return NotFound();
+            }
+
+            courseRequest.IsApproved = true;
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
