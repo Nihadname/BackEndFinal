7bd66c52bf72d52556d847fc9ba08a29e8a5dbbb
Nihadname <nihadcoding@gmail.com>
2024-08-03 13:26:57 +0400

    erfrefefer

--- BackEndFinal/Areas/AdminArea/Controllers/SpeakerController.cs
+++ BackEndFinal/Areas/AdminArea/Controllers/SpeakerController.cs
@@ -30,15 +30,15 @@ namespace BackEndFinal.Areas.AdminArea.Controllers
         }
         public async Task<IActionResult> Detail(int? id)
         {
-            if(id == null) return BadRequest();
-            var existedSpeaker=await speakerService.GetSpeakerByIdAsync(id,s=>s.Event);
-            if(existedSpeaker  == null) return NotFound();
+            if (id == null) return BadRequest();
+            var existedSpeaker = await speakerService.GetSpeakerByIdAsync(id, s => s.Event);
+            if (existedSpeaker == null) return NotFound();
             return View(existedSpeaker);
 
         }
         public async Task<IActionResult> Create()
         {
-            ViewBag.Events = new SelectList(await eventService.GetAllEventAsync(0, 0,s=>s.Speakers), "Id", "Title");
+            ViewBag.Events = new SelectList(await eventService.GetAllEventAsync(0, 0, s => s.Speakers), "Id", "Title");
             return View();
         }
 
@@ -61,12 +61,12 @@ namespace BackEndFinal.Areas.AdminArea.Controllers
                     return RedirectToAction(nameof(Index));
                 }
                 speaker.ImageUrl = await newProfileImage.SaveFile("teacher");
-             
+
             }
-          
+
             speaker.Name = speakerCreateVM.Name;
             speaker.EventId = speakerCreateVM.EventId;
-            speaker.position  = speakerCreateVM.Position;
+            speaker.position = speakerCreateVM.Position;
             await speakerService.AddSpeakerAsync(speaker);
             return RedirectToAction("Index");
         }
@@ -80,6 +80,52 @@ namespace BackEndFinal.Areas.AdminArea.Controllers
             await speakerService.DeleteSpeakerAsync(existedSpeaker);
             return RedirectToAction(nameof(Index));
         }
+        public async Task<IActionResult> Update(int? id)
+        {
+            ViewBag.Events = new SelectList(await eventService.GetAllEventAsync(0, 0, s => s.Speakers), "Id", "Title");
 
+            if (id == null) return BadRequest();
+            var existedSpeaker = await speakerService.GetSpeakerByIdAsync(id, s => s.Event);
+            if (existedSpeaker == null) return NotFound();
+            return View(new SpeakerUpdateVM
+            {
+                Name = existedSpeaker.Name,
+                EventId = existedSpeaker.EventId,
+                Position = existedSpeaker.position,
+            });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id, SpeakerUpdateVM model)
+        {
+            ViewBag.Events = new SelectList(await eventService.GetAllEventAsync(0, 0, s => s.Speakers), "Id", "Title");
+
+            if (id == null) return BadRequest();
+            var existedSpeaker = await speakerService.GetSpeakerByIdAsync(id, s => s.Event);
+            if (existedSpeaker == null) return NotFound();
+            if(!ModelState.IsValid) return  View(model);
+            var newProfileImage = model.Photo;
+            if (newProfileImage != null)
+            {
+                if (!newProfileImage.CheckContentType())
+                {
+                    ModelState.AddModelError("photo", "Only image files are allowed.");
+                    return RedirectToAction(nameof(Index));
+                }
+
+                if (!string.IsNullOrEmpty(existedSpeaker.ImageUrl))
+                {
+                    existedSpeaker.ImageUrl.DeleteFile("teacher");
+                }
+
+                existedSpeaker.ImageUrl = await newProfileImage.SaveFile("teacher");
+               
+            }
+
+            existedSpeaker.position = model.Position;
+            existedSpeaker.Name = model.Name;
+            existedSpeaker.EventId = model.EventId;
+            await speakerService.UpdateSpeakerAsync(existedSpeaker);
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ BackEndFinal/ViewModels/SpeakerUpdateVM.cs
@@ -0,0 +1,10 @@
+﻿namespace BackEndFinal.ViewModels
+{
+    public class SpeakerUpdateVM
+    {
+        public string Name { get; set; }
+        public IFormFile Photo { get; set; }
+        public string Position { get; set; }
+        public int EventId { get; set; }
+    }
+}
